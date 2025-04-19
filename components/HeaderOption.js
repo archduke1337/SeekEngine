5729448c2520ceb714f019eb075b0f1a25e5c394
Shresth <shresthramyadav@gmail.com>
2025-04-19 17:41:15 +0530

    Update HeaderOption.js

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -1,16 +1,16 @@
 function HeaderOption({ Icon, title, selected, onClick }) {
     return (
         <div
-            onClick={onClick} // Optional: If you want it to be interactive (clickable)
+            onClick={onClick}  // Make it clickable
             className={`flex items-center space-x-2 border-b-4 border-transparent cursor-pointer 
                 hover:text-blue-500 hover:border-blue-500 
                 ${selected ? "text-blue-500 border-blue-500" : ""}
                 transition-all duration-200 ease-in-out pb-3`}
-            aria-label={title} // Improve accessibility
-            role="button" // Make it semantically a button for interaction
+            aria-label={title} // Make it accessible
+            role="button" // Make it semantically a button for assistive tech
         >
             <Icon className="h-5" />
-            <p className="hidden sm:inline-flex">{title}</p> {/* Only visible on sm+ screens */}
+            <p className="hidden sm:inline-flex">{title}</p>
         </div>
     );
 }

89dbc929715e5c196d1968c998538e9d5f642a1d
Shresth <shresthramyadav@gmail.com>
2025-04-19 17:38:15 +0530

    Update HeaderOptions.js

--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import HeaderOption from "./HeaderOption";
-
 import {
     DotsVerticalIcon,
     MapIcon,
@@ -10,22 +10,62 @@ import {
 } from "@heroicons/react/outline";
 
 function HeaderOptions() {
+    const [selectedOption, setSelectedOption] = useState("All");
+
+    const handleOptionClick = (option) => {
+        setSelectedOption(option);
+    };
+
     return (
         <div className="flex w-full flex-col sm:flex-row justify-between items-center text-gray-700 dark:text-gray-300 text-sm lg:text-base border-b px-4 lg:pl-52">
             {/* Left Options */}
             <div className="flex flex-wrap justify-center sm:justify-start space-x-6 lg:space-x-10 py-2">
-                <HeaderOption Icon={SearchIcon} title="All" selected />
-                <HeaderOption Icon={PhotographIcon} title="Images" />
-                <HeaderOption Icon={PlayIcon} title="Videos" />
-                <HeaderOption Icon={NewspaperIcon} title="News" />
-                <HeaderOption Icon={MapIcon} title="Maps" />
-                <HeaderOption Icon={DotsVerticalIcon} title="More" />
+                <HeaderOption
+                    Icon={SearchIcon}
+                    title="All"
+                    selected={selectedOption === "All"}
+                    onClick={() => handleOptionClick("All")}
+                />
+                <HeaderOption
+                    Icon={PhotographIcon}
+                    title="Images"
+                    selected={selectedOption === "Images"}
+                    onClick={() => handleOptionClick("Images")}
+                />
+                <HeaderOption
+                    Icon={PlayIcon}
+                    title="Videos"
+                    selected={selectedOption === "Videos"}
+                    onClick={() => handleOptionClick("Videos")}
+                />
+                <HeaderOption
+                    Icon={NewspaperIcon}
+                    title="News"
+                    selected={selectedOption === "News"}
+                    onClick={() => handleOptionClick("News")}
+                />
+                <HeaderOption
+                    Icon={MapIcon}
+                    title="Maps"
+                    selected={selectedOption === "Maps"}
+                    onClick={() => handleOptionClick("Maps")}
+                />
+                <HeaderOption
+                    Icon={DotsVerticalIcon}
+                    title="More"
+                    selected={selectedOption === "More"}
+                    onClick={() => handleOptionClick("More")}
+                />
             </div>
 
             {/* Right Options */}
             <div className="flex space-x-4 py-2">
-                <p className="link cursor-pointer hover:underline">Settings</p>
-                <p className="link cursor-pointer hover:underline">Tools</p>
+                <button className="link cursor-pointer hover:underline focus:outline-none" aria-label="Settings">
+                    Settings
+                </button>
+                <button className="link cursor-pointer hover:underline focus:outline-none" aria-label="Tools">
+                    Tools
+                </button>
             </div>
         </div>
     );

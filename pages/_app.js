ecdadaf329a399458d125831e0efaedf432d5965
archduke1337 <gauravramyadav@gmail.com>
2025-10-20 18:14:54 +0530

    Add Vercel Web Analytics integration
    Integrated @vercel/analytics to enable web analytics tracking. Updated package.json with the new dependency and modified _app.js to include the Analytics component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import ThemeProvider from '../components/ThemeProvider';
 import { validateEnvironment } from '../utils/env';
+import { Analytics } from '@vercel/analytics/react';
 
 // Validate environment variables on server startup
 if (typeof window === 'undefined') {
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
